578e7b589a806d19e6d18cf8649ae3ef2eae3ca0
Ian Sutton <ian.sutton.snow@gmail.com>
2025-07-04 20:02:30 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ShoppingListClient.tsx
+++ src/components/ShoppingListClient.tsx
@@ -14,6 +14,7 @@ import { Skeleton } from './ui/skeleton';
 
 export default function ShoppingListClient({ storeId }: { storeId: string }) {
   const {
+    stores,
     getStore,
     addItem,
     toggleItem,
